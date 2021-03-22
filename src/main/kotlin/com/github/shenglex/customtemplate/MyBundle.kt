b0b9abdb142afb12c0288eda6cd30d0008e9526f
GitHub Action <action@github.com>
2021-03-22 09:17:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/shenglex/customtemplate/MyBundle.kt
+++ src/main/kotlin/com/github/shenglex/customtemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.shenglex.customtemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/shenglex/customtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shenglex/customtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shenglex.customtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shenglex.customtemplate.MyBundle
 
 class MyApplicationService {
 
